b6aca5e1d86c8ec0f6010f81f9652d7b8afa2938
Everunity <everunity8@gmail.com>
2025-06-19 16:01:10 +0400

    Update project code

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,15 @@ function App() {
     <>
       <h1 className="text-3xl font-bold underline text-black">Hello React</h1>
       <p>this is the new project </p>
+      <div className="bg-red-500">
+        <h1 className="text-3xl font-bold underline text-black">Hello world</h1>
+      </div>
+      <div className="bg-green-500">
+        <h1 className="text-3xl font-bold underline text-black">Hello green</h1>
+      </div>
+      <div className="bg-blue-500">
+        <h1 className="text-3xl font-bold underline text-black">Hello blue</h1>
+      </div>
     </>
   );
 }
