5f7634ecb30a59d7a3023347eba9db6ce5fd8fc9
krupaGetMax <krupa@getmax.ae>
2025-06-19 14:34:55 +0400

    Replaced code with latest JS version

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import "./App.css";
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline text-black">Hello React</h1>
+    </>
+  );
+}
+
+export default App;
