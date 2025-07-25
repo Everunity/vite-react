290c9a95bf512bd79dd7c4261153efe468b53090
Sagar Muneshwar <129708548+Sagar1006-dev@users.noreply.github.com>
2025-07-26 01:03:50 +0530

    ScrollToTop.jsx

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import Header from "./Components/Header/Header.jsx"
 import Footer from './Components/Footer/Footer.jsx'
 import { Outlet } from 'react-router-dom'
+import ScrollToTop from './ScrollToTop.jsx'
 const Layout = () => {
   return (
     <>
     <Header/>
     <Outlet/>
+    <ScrollToTop />
     <Footer/>
     </>
   )
